863ee9b0dd555e8bf949b64f55bfb2763cb806ae
ZenFrontender <ks29609@gmail.com>
2025-08-18 03:19:33 +0530

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const {initializeDatabase} = require('./db/db.connect');
 const Hotel = require('./models/hotel.models');
 const express = require('express')
 const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
